872a0c856559788dcb7d6152a96104d99ad8399c
Glenon Mateus <glenonmateus+github@gmail.com>
2025-02-06 12:41:06 -0300

    refactor: add localstack provider endpoints

--- main.tf
+++ main.tf
@@ -5,6 +5,11 @@ provider "aws" {
   skip_credentials_validation = true
   skip_requesting_account_id  = true
   skip_metadata_api_check     = true
+  endpoints {
+    dynamodb = "http://localhost:4566"
+    ec2      = "http://localhost:4566"
+    s3       = "http://s3.localhost.localstack.cloud:4566"
+  }
 }
 
 terraform {
